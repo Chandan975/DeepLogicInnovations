80e204179ba10d6c7b78b335c3e8d2bbe2dc08a4
chandan975 <chandanbagwan7@gmail.com>
2023-10-01 21:49:19 +0530

    update code

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,11 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),  
+    path('about', views.about, name='about'),
+    path('service', views.service, name='service'),
+    path('project', views.project, name='project'),
+    path('contact', views.contact, name='contact'),
+    path('blog', views.blog, name='blog'),
+    path('team', views.team, name='team'),
+    path('testimonial', views.testimonial, name='testimonial'),    
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,15 @@ def index(request):
     return render(request ,"index.html")
 def about(request):
     return render(request ,"about.html")
+def service(request):
+    return render(request ,"service.html")
+def project(request):
+    return render(request ,"project.html")
+def contact(request):
+    return render(request ,"contact.html")
+def blog(request):
+    return render(request ,"blog.html")
+def team(request):
+    return render(request ,"team.html")
+def testimonial(request):
+    return render(request ,"testimonial.html")
